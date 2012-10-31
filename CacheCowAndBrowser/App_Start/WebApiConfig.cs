8033eb2aec63d10908458d8dce24081c20056fb0
Pete Smith <pete.smith@oneandthreeconsulting.co.uk>
2012-10-31 11:02:29 +0000

    Added solution to GitHub

---
+++ CacheCowAndBrowser/App_Start/WebApiConfig.cs
@@ -0,0 +1,35 @@
+﻿using System.Web.Http;
+
+namespace CacheCowAndBrowser
+{
+    using System;
+    using System.Net.Http.Headers;
+
+    using CacheCow.Server;
+
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{action}");
+
+            //config.MessageHandlers.Add(new CachingHandler());
+
+            // Comment this in to make IE work correctly
+
+            config.MessageHandlers.Add(
+                 new CachingHandler
+                 {
+                     CacheControlHeaderProvider = message => new CacheControlHeaderValue
+                     {
+                         Private = true,
+                         MustRevalidate = true,
+                         NoTransform = true,
+                         MaxAge = TimeSpan.Zero
+                     }
+                 });
+        }
+    }
+}
---
+++ CacheCowAndBrowser/Controllers/ValuesController.cs
@@ -0,0 +1,59 @@
+﻿using System.Net.Http;
+using System.Web.Http;
+
+namespace CacheCowAndBrowser.Controllers
+{
+    using System.Net.Http.Formatting;
+
+    public class MyResponse
+    {
+        public string Name { get; set; }
+
+        public string Title { get; set; }
+    }
+
+    public class ValuesController : ApiController
+    {
+        private MyResponse obj;
+
+        public ValuesController()
+        {
+            this.obj = new MyResponse() { Name = "Test", Title = "Test" };
+        }
+
+        public HttpResponseMessage GetDefaultHeaders()
+       {
+           var response = new HttpResponseMessage()
+               {
+                   Content = new ObjectContent<MyResponse>(obj, new JsonMediaTypeFormatter())
+               };
+
+           return response;
+       }
+
+        public HttpResponseMessage GetWithNoCache()
+        {
+            var response = new HttpResponseMessage()
+            {
+                Content = new ObjectContent<MyResponse>(obj, new JsonMediaTypeFormatter())
+            };
+
+            response.Headers.Add("Cache-Control", "no-cache");
+
+            return response;
+        }
+
+        public HttpResponseMessage GetWithPragmaExpires()
+        {
+            var response = new HttpResponseMessage()
+            {
+                Content = new ObjectContent<MyResponse>(obj, new JsonMediaTypeFormatter())
+            };
+
+            response.Headers.Add("Pragma", "no-cache");
+            response.Content.Headers.TryAddWithoutValidation("Expires", "-1");
+
+            return response;
+        }
+    }
+}
